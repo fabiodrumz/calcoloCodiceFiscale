5d6539b79b29492e6e932803b4a807900863f1fb
buffogiorgio <buffo.giorgio04@gmail.com>
2024-05-09 12:26:52 +0200

    fix per calcolare le prime 11 cifre del codice fiscale

--- codiceFiscale/codiceFiscale.cpp
+++ codiceFiscale/codiceFiscale.cpp
@@ -1,60 +1,93 @@
 #include <iostream>
 #include <string>
+#include <cctype>
+#include <algorithm> // Per utilizzare la funzione transform
 
 using namespace std;
 
-string calcolaCodiceFiscale(string nome, string cognome, int giorno, int mese, int anno, char sesso) {
-    // Array di vocali per il calcolo del codice fiscale
-    const char vocali[] = { 'A', 'E', 'I', 'O', 'U' };
+// Funzione ausiliaria per contare le consonanti in una stringa
+int contaConsonanti(const string& s) {
+    const string vocali = "AEIOU";
+    int count = 0;
+    for (char c : s) {
+        if (isalpha(c) && vocali.find(toupper(c)) == string::npos) {
+            count++;
+        }
+    }
+    return count;
+}
 
-    // Costruzione del codice fiscale
-    string codiceFiscale = "";
+// Funzione per calcolare il codice fiscale
+string calcolaCodiceFiscale(const string& cognome, const string& nome, const string& dataNascita, char genere) {
+    // Conversione delle stringhe in maiuscolo
+    string cognomeMaiuscolo = cognome;
+    string nomeMaiuscolo = nome;
+    transform(cognomeMaiuscolo.begin(), cognomeMaiuscolo.end(), cognomeMaiuscolo.begin(), ::toupper);
+    transform(nomeMaiuscolo.begin(), nomeMaiuscolo.end(), nomeMaiuscolo.begin(), ::toupper);
 
-    // Parte del codice fiscale derivata dal cognome
-    codiceFiscale += cognome.substr(0, 3);
-
-    // Parte del codice fiscale derivata dal nome
-    int contatore = 0;
-    for (char lettera : nome) {
-        if (contatore == 3)
-            break;
-        if (lettera != 'A' && lettera != 'E' && lettera != 'I' && lettera != 'O' && lettera != 'U') {
-            codiceFiscale += lettera;
-            contatore++;
+    // Estrazione delle consonanti dal cognome
+    string consonantiCognome = "";
+    for (char c : cognomeMaiuscolo) {
+        if (isalpha(c) && string("BCDFGHJKLMNPQRSTVWXYZ").find(c) != string::npos) {
+            consonantiCognome += c;
+            if (consonantiCognome.length() == 3) {
+                break;
+            }
         }
     }
 
-    // Parte del codice fiscale derivata dalla data di nascita
-    codiceFiscale += to_string(anno % 100); // Anno solo con le ultime due cifre
-    codiceFiscale += (mese < 10) ? '0' + to_string(mese) : to_string(mese); // Mese
-    codiceFiscale += (giorno < 10) ? '0' + to_string(giorno) : to_string(giorno); // Giorno
+    // Estrazione delle consonanti dal nome
+    string consonantiNome = "";
+    int numConsonanti = 0;
+    for (char c : nomeMaiuscolo) {
+        if (isalpha(c) && string("BCDFGHJKLMNPQRSTVWXYZ").find(c) != string::npos) {
+            consonantiNome += c;
+            numConsonanti++;
+            if (numConsonanti == 3) {
+                break;
+            }
+        }
+    }
 
-    // Parte del codice fiscale derivata dal sesso
-    codiceFiscale += sesso;
+    // Estrazione ultime due cifre dell'anno di nascita
+    string anno = dataNascita.substr(2, 2);
+
+    // Estrazione del mese di nascita
+    const string mesi = "ABCDEHLMPRST";
+    int mese = stoi(dataNascita.substr(5, 2));
+    char meseNascita = mesi[mese - 1];
+
+    // Estrazione del giorno di nascita (con eventuale aggiustamento per genere femminile)
+    int giorno = stoi(dataNascita.substr(8, 2));
+    if (genere == 'F') {
+        giorno += 40;
+    }
+    string giornoNascita = (giorno < 10) ? "0" + to_string(giorno) : to_string(giorno);
+
+    // Costruzione del codice fiscale
+    string codiceFiscale = consonantiCognome + consonantiNome + anno + meseNascita + giornoNascita;
 
     return codiceFiscale;
 }
 
 int main() {
-    string nome, cognome;
-    int giorno, mese, anno;
-    char sesso;
 
-    cout << "Inserisci il nome: ";
-    cin >> nome;
+    setlocale(LC_ALL, "italian");
+
+    string cognome, nome, dataNascita;
+    char genere;
+
     cout << "Inserisci il cognome: ";
     cin >> cognome;
-    cout << "Inserisci il giorno di nascita: ";
-    cin >> giorno;
-    cout << "Inserisci il mese di nascita: ";
-    cin >> mese;
-    cout << "Inserisci l'anno di nascita: ";
-    cin >> anno;
-    cout << "Inserisci il sesso (M per maschio, F per femmina): ";
-    cin >> sesso;
+    cout << "Inserisci il nome: ";
+    cin >> nome;
+    cout << "Inserisci la data di nascita secondo il formato 'AAAA-MM-GG': ";
+    cin >> dataNascita;
+    cout << "Inserisci il genere (M per maschio, F per femmina): ";
+    cin >> genere;
 
-    string codiceFiscale = calcolaCodiceFiscale(nome, cognome, giorno, mese, anno, sesso);
-    cout << "Il codice fiscale calcolato è: " << codiceFiscale << endl;
+    string codiceFiscale = calcolaCodiceFiscale(cognome, nome, dataNascita, genere);
+    cout << "Il codice fiscale generato è: " << codiceFiscale << endl;
 
     return 0;
 }
